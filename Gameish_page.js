266f72161c27d9ca6a2f9ced64649829d4cdc210
Ahishayan <78314633+Ahishayan@users.noreply.github.com>
2021-09-15 18:23:02 -0400

    Update Gameish_page.js

--- Gameish_page.js
+++ Gameish_page.js
@@ -42,14 +42,14 @@ function check()
 	{
 		if(answer_turn == "player1")
 		{
-			player1_score = player1_score+1;
-		    document.getElementById("player1_score").innerHTML = player1_score;
+			player2_score = player2_score+1;
+		    document.getElementById("player2_score").innerHTML = player1_score;
             
 		}
 		else 
 		{
-			player2_score = player2_score+1;
-		    document.getElementById("player2_score").innerHTML = player2_score;
+			player1_score = player1_score+1;
+		    document.getElementById("player1_score").innerHTML = player2_score;
 		}
 	}
 	

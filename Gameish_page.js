0cd86bea8114f64389bd68a831f47e89b88911ff
Ahishayan <78314633+Ahishayan@users.noreply.github.com>
2021-11-24 15:30:24 -0500

    Update Gameish_page.js

--- Gameish_page.js
+++ Gameish_page.js
@@ -4,7 +4,7 @@
 
 	player1_score = 0;
 	player2_score = 0;
-    actual_answer=0;
+
 document.getElementById("player1_name").innerHTML = player1_name + " : ";
 document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
@@ -36,20 +36,18 @@ answer_turn = "player2";
 
 function check()
 {
-    
 	get_answer = document.getElementById("input_check_box").value;
 	if(get_answer == actual_answer)	
 	{
 		if(answer_turn == "player1")
 		{
-			player2_score = player2_score+1;
-		    document.getElementById("player2_score").innerHTML = player1_score;
-            
+			player1_score = player1_score +1;
+		    document.getElementById("player1_score").innerHTML = player1_score;
 		}
 		else 
 		{
-			player1_score = player1_score+1;
-		    document.getElementById("player1_score").innerHTML = player2_score;
+			player2_score = player2_score +1;
+		    document.getElementById("player2_score").innerHTML = player2_score;
 		}
 	}
 	
@@ -77,3 +75,4 @@ function check()
 
     document.getElementById("output").innerHTML = "";
 }
+
